cbb53a236e5746dc65caa553ff4fefa2ba3615e6
stla <laurent_step@yahoo.fr>
2018-10-23 14:47:01 +0200

    code cleaning

--- src/Torus2/Triangles.hs
+++ src/Torus2/Triangles.hs
@@ -3,7 +3,7 @@ module Torus2.Triangles
   where
 import           Graphics.Rendering.OpenGL.GL (Normal3 (..), Vertex3 (..))
 import           Data.Array                   (Array, (!), array)
-import qualified Data.Array as UA
+import qualified Data.Array as A
 
 type Point = (Double, Double, Double)
 type Vector = (Double, Double, Double)
@@ -17,7 +17,7 @@ vectorToNormal3 :: Vector -> Normal3 Double
 vectorToNormal3 (x,y,z) = Normal3 x y z
 
 fun :: Double -> Double -> Double -> Double -> Point
-fun a nlobes u v = (x,y,z)
+fun a nlobes u v = (x1/(den-x4), x2/(den-x4), x3/(den-x4))
   where
     a1 = pi/2 - (pi/2-a) * cos(u*nlobes)
     a2 = u + a*sin(2*u*nlobes)
@@ -25,22 +25,19 @@ fun a nlobes u v = (x,y,z)
     p1 = cos a1
     p2 = sina1 * cos a2
     p3 = sina1 * sin a2
-    yden = sqrt(2*(1+p1))
     cosphi = cos v
     sinphi = sin v
     x4 = cosphi * (1+p1)
     x3 = sinphi * (1+p1)
     x2 = cosphi*p2 - sinphi*p3
     x1 = cosphi*p3 + sinphi*p2
-    x = x1/(yden-x4)
-    y = x2/(yden-x4)
-    z = x3/(yden-x4)
+    den = sqrt(2*(1+p1))
 
 frac :: Int -> Int -> Double
 frac p q = realToFrac p / realToFrac q
 
 allVertices :: (Double -> Double -> Point) -> (Int,Int) -> Array (Int,Int) Point
-allVertices f (nu, nv) = UA.array ((0,0), (nu-1,nv-1)) associations
+allVertices f (nu, nv) = array ((0,0), (nu-1,nv-1)) associations
   where
   u_ = [2*pi * frac i nu | i <- [0 .. nu-1]]
   v_ = [2*pi * frac i nv | i <- [0 .. nv-1]]
@@ -62,7 +59,7 @@ averageNormals (x1,y1,z1) (x2,y2,z2) (x3,y3,z3) (x4,y4,z4) (x5,y5,z5) (x6,y6,z6)
 normalij :: Array (Int,Int) Point -> (Int, Int) -> Vector
 normalij vertices (i,j) = averageNormals n1 n2 n3 n4 n5 n6
   where
-  ((_,_), (nu',nv')) = UA.bounds vertices
+  ((_,_), (nu',nv')) = A.bounds vertices
   im1 = if i==0 then nu' else i-1
   ip1 = if i==nu' then 0 else i+1
   jm1 = if j==0 then nv' else j-1
@@ -75,10 +72,10 @@ normalij vertices (i,j) = averageNormals n1 n2 n3 n4 n5 n6
   n6 = triangleNormal0 (vertices ! (i,j), vertices ! (im1,jp1), vertices ! (i,jp1))
 
 allNormals :: Array (Int,Int) Point -> Array (Int,Int) Vector
-allNormals vertices = UA.array bounds associations
+allNormals vertices = array bounds associations
   where
-  bounds = UA.bounds vertices
-  indices = UA.indices vertices  
+  bounds = A.bounds vertices
+  indices = A.indices vertices  
   g (i,j) = ((i,j), normalij vertices (i,j))
   associations = map g indices
 
